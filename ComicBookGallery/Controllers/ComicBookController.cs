b0ee01d3c8904ba35f27d09ef2d58ffba5062cc2
golfa <golfa@LAPTOP-9QGEIVQK>
2017-02-05 10:53:04 +0000

    created html file

--- ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -6,18 +6,11 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBookController : Controller
+    public class ComicBooksController : Controller
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-
-
-            return Content("Hello from comic book controller!");
+            return View();
 
 
         }
